9281f4f9508a59b5a9020d84f3c1a47368150532
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-02-12 10:59:15 -0500

    Add user type

---
+++ src/user.ts
@@ -0,0 +1,128 @@
+/**
+ * The user as expressed by Azure AD B2C.
+ */
+export default interface User {
+  /**
+   * This claim identifies the security token service (STS) that constructs and returns the token.
+   * It also identifies the Azure AD directory in which the user was authenticated.
+   * Your app should validate the issuer claim to ensure that the token came from the v2.0 endpoint.
+   * It also should use the GUID portion of the claim to restrict the set of tenants that can sign in to the app.
+   */
+  iss: string;
+
+  /**
+   * This is the name of the policy that was used to acquire the token.
+   */
+  tfp: string;
+
+  /**
+   * The version of the ID token, as defined by Azure AD B2C.
+   */
+  ver: string;
+
+  /**
+   * A nonce is a strategy used to mitigate token replay attacks.
+   * Your app can specify a nonce in an authorization request by using the nonce query parameter.
+   * The value you provide in the request will be emitted unmodified in the nonce claim of an ID token only.
+   * This allows your app to verify the value against the value it specified on the request,
+   * which associates the app's session with a given ID token.
+   * Your app should perform this validation during the ID token validation process.
+   */
+  nonce: string;
+
+  /**
+   * The expiration time claim is the time at which the token becomes invalid, represented in epoch time.
+   * Your app should use this claim to verify the validity of the token lifetime.
+   */
+  exp: number;
+
+  /**
+   * This claim is the time at which the token becomes valid, represented in epoch time.
+   * This is usually the same as the time the token was issued.
+   * Your app should use this claim to verify the validity of the token lifetime.
+   */
+  nbf: number;
+
+  /**
+   * The time at which the token was issued, represented in epoch time.
+   */
+  iat: number;
+
+  /**
+   * This claim is the time at which a user last entered credentials, represented in epoch time.
+   */
+  auth_time: number;
+
+  /**
+   * An audience claim identifies the intended recipient of the token.
+   * For Azure AD B2C, the audience is your app's Application ID, as assigned to your app in the app registration portal.
+   * Your app should validate this value and reject the token if it does not match.
+   */
+  aud: string;
+
+  /**
+   * This is the principal about which the token asserts information, such as the user of an app.
+   * This value is immutable and cannot be reassigned or reused.
+   * It can be used to perform authorization checks safely, such as when the token is used to access a resource.
+   * By default, the subject claim is populated with the object ID of the user in the directory.
+   */
+  sub: string;
+
+  /**
+   * The immutable identifier for the user account in the tenant.
+   * It can be used to perform authorization checks safely and as a key in database tables.
+   * This ID uniquely identifies the user across applications.
+   * Two different applications signing in the same user will receive the same value in the oid claim.
+   * This means that it can be used when making queries to Microsoft online services, such as the Microsoft Graph.
+   * The Microsoft Graph will return this ID as the id property for a given user account.
+   */
+  oid: string;
+
+  /**
+   * Email addresses of the user.
+   * These are mutable and might change over time.
+   * Therefore, they are not suitable for identifying the user in other databases or applications.
+   * The oid or sub claim should be used instead.
+   */
+  emails: string[];
+
+  /**
+   * The user's full name in displayable form including all name parts, possibly including titles and suffixes.
+   */
+  name: string;
+
+  /**
+   * The user's given name (also known as first name).
+   */
+  given_name: string;
+
+  /**
+   * The user's surname (also known as last name).
+   */
+  family_name: string;
+
+  /**
+   * The street address where the user is located.
+   */
+  streetAddress: string;
+
+  /**
+   * The city in which the user is located.
+   */
+  city: string;
+
+  /**
+   * The state or province in which the user is located.
+   */
+  state: string;
+
+  /**
+   * The postal code of the user's address.
+   */
+  postalCode: string;
+
+  /**
+   * The country in which the user is located.
+   */
+  country: string;
+}
