b40250da17ea966627a1ae0f436c2e774c396177
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-02-23 04:33:11 -0500

    Add copy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ * This code can only be used and/or distributed with express permission.
+ */
+
 import type User from './user';
 import type ItemInventory from './item-inventory';
 import type Ref from './ref';
