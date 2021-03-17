c10fbf2cfecae65673e1a6db57c5eec79990b4d1
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-03-17 07:34:52 -0400

    update comment

--- src/eshop/cart-item.ts
+++ src/eshop/cart-item.ts
@@ -6,6 +6,7 @@ import CosmosBase from '../types/extends/cosmos-base';
 export default interface CartItem extends CosmosBase {
   /**
    * The current number of an inventory item requested for purchase.
+   * The id specifies the inventory item's id while the Discriminator specifies the customer's id.
    */
   quantity: number;
 }
