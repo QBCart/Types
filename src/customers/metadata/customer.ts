d6f94a2d84138c89897dd4c83411d10bed12a48b
Allan Mobley <allan.mobley.jr@qbcart.net>
2021-08-12 07:35:11 +0000

    add current QBCart metadata

--- src/customers/metadata/customer.ts
+++ src/customers/metadata/customer.ts
@@ -1,4 +1,24 @@
 /**
  * Describes metadata that QBCart tracks in regards to "Customer" list objects from QuickBooks.
  */
-export default interface Metadata {}
+export default interface Metadata {
+  /**
+   * Whether customer is past due on payment terms.
+   */
+  PastDue: boolean;
+
+  /**
+   * Whether a recent payment was declined.
+   */
+  DeclinedPayment: boolean;
+
+  /**
+   * Reformatted version of FullName used mainly for search and in search results.
+   */
+  LongName: string;
+
+  /**
+   * Abbreviated version of address used mainly for search and in search results.
+   */
+  Location: string;
+}
